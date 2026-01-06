ca2a86da16aab91534a00fb90699ef648ebd9fcb
Hever Kasta <heverkasta.dv@gmail.com>
2026-01-06 17:56:28 -0300

    Versão Alfa 06/01
    Sistema de criação do mapa adicionado.
    Inimigos que seguem o player adicionados.
    Melhoria na animação do player.

--- Soul Hunter/scripts/elise.gd
+++ Soul Hunter/scripts/elise.gd
@@ -4,19 +4,29 @@ extends CharacterBody2D
 @onready var attack_joystick: Control = %"Attack Joystick".get_node("base")
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var speed := 150.0
+@export var speed := 50.0
 
 func _physics_process(_delta: float):
-	var direction = move_joystick.get_direction()
-	velocity = direction * speed
+	var attack_direction = attack_joystick.get_direction()
+	var move_direction = move_joystick.get_direction()
+	velocity = move_direction * speed
 	move_and_slide()
 	
-	if direction.x > 0:
+	if attack_direction.x > 0:
 		animation.flip_h = false
-	elif direction.x < 0:
+	elif attack_direction.x < 0:
 		animation.flip_h = true
 	
 	if velocity.length() == 0.0:
 		animation.play("idle")
 	else:
-		animation.play("run")
+		if move_direction.x > 0:
+			if animation.flip_h == false:
+				animation.play("run")
+			else:
+				animation.play_backwards("run")
+		elif move_direction.x < 0:
+			if animation.flip_h == true:
+				animation.play("run")
+			else:
+				animation.play_backwards("run")
---
+++ Soul Hunter/scripts/slime_1.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var elise: CharacterBody2D = %Elise
+
+@export var speed := 0.0
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(elise.global_position)
+	velocity = direction * speed
+	move_and_slide()
+	
+	if direction.x > 0:
+		animation.flip_h = false
+	elif direction.x < 0:
+		animation.flip_h = true
